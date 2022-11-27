6fa342ba58b3ecf1c656356f66a3223aa230b4c6
Vanessa <faye_3091@yahoo.com>
2022-11-27 01:16:55 -0500

    sit up models

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,8 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const Category = require("./Category");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,13 +11,50 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    //id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //product_name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    //price column
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    //stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    //catergory_id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,43 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const Product = require("./Product");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    //define id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //define product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: "id",
+      },
+    },
+    //define tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
