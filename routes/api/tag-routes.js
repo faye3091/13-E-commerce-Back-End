f66aca50456e5b48114ebb57c7cd256111e118cf
Vanessa <faye_3091@yahoo.com>
2022-12-07 16:05:35 -0500

    updated routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,36 @@ const { Tag, Product, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
 });
 
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
 });
 
 router.post("/", (req, res) => {
